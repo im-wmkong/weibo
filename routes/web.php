4ad95bb49f14dc58f551adaf97e69634b1cf601d
KongWeiMin <kwm672225801@gmail.com>
2019-05-22 21:42:13 +0800

    用户激活

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,5 @@ Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::get('signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
 Route::resource('users', 'UsersController');
