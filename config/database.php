9ee962327718305a77e1a8e132b53a90ff227a0e
KongWeiMin <kongweimin@gooagoo.com>
2019-06-24 17:11:23 +0800

    test email

--- config/database.php
+++ config/database.php
@@ -63,11 +63,11 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => $db_config['host'],
-            'port'     => env('DB_PORT', '5432'),
-            'database' => $db_config['database'],
-            'username' => $db_config['username'],
-            'password' => $db_config['password'],
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
